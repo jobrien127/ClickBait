3883b50d1c8b743fad8e2a9f1d13fe75199894b7
Nathaniel Fuller <nfuller@mymail.mines.edu>
2018-10-11 07:10:15 -0600

    Added pseudo-pointless splash generator to enhance the ClickBait experience.

--- clickbait.js
+++ clickbait.js
@@ -1,6 +1,17 @@
 $(document).ready(function() {
 	update_scores(); // Call this on page load to update high-scores list.
 	
+	// Pick a random click-bait-y splash text
+	// If anyone thinks of anything else you want to add, feel free. Just make sure to alter the hard-coded values for the random selection below.
+	splashTextA = ["Top 10 Scores", "These 8 Gameplay Photos", "12 Most Shocking Dots"];
+	splashTextB = [" That You Won't Believe", " That Professor Paone Doesn't Want You To Know About", " Too Ridiculous To Be True"];
+	splashTextC = [" Will Shock You!", ": Number 5 Will Surprise You!", " Will Blow Your Mind!", " Will Change Your Life!"];
+	
+	// -- Compile Random Splash --
+	splashString = splashTextA[Math.floor(Math.random() * 3)] + splashTextB[Math.floor(Math.random() * 3)] + splashTextC[Math.floor(Math.random() * splashTextC.length)];
+	
+	$("#splashText").text(splashString);
+	
 	// highscore(score)
 	// ^^ Format for calling function to submit a potential high score.
 });
