19036254f28ae16ef0ded14b5bbfb78c28b295d2
Nathaniel Fuller <nfuller@mymail.mines.edu>
2018-10-11 03:24:44 -0600

    Edited clickbait.js to add initial function calls.

---
+++ clickbait.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+	update_scores(); // Call this on page load to update high-scores list.
+	
+	// highscore(score)
+	// ^^ Format for calling function to submit a potential high score.
+});
